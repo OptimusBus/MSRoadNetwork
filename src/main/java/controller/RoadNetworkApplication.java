71fe9600c93b86ebe2d5853256e4dc5d7c63f2b1
augusto ungolo <augustoungolo@MacBook-Air-di-Augusto.local>
2021-11-14 19:33:26 +0100

    fixed

--- src/main/java/controller/RoadNetworkApplication.java
+++ src/main/java/controller/RoadNetworkApplication.java
@@ -6,7 +6,7 @@ import java.util.Set;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("/RoadNetworkApplication-1.0")
+@ApplicationPath("/RoadNetwork-1.0")
 public class RoadNetworkApplication extends Application{
 	
 	public Set<Class<?>> getClasses() {
